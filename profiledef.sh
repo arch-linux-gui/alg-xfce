e5d48fdfb349eb897f1e72347770d03f252a6fba
demonkiller2 <hiranyagarbh7@gmail.com>
2021-05-21 09:59:17 +0530

    added gitignore and skel

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-gui-xfce-pure"
+iso_name="archlinux-gui-xfce"
 iso_label="ARCH_GUI_XFCE_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
