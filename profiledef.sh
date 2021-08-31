150d4ccf4c3a3e9d7f868ddec49e8f0412cd14de
demonkiller <gaurangvishwakarma619@gmail.com>
2021-09-01 00:09:20 +0530

    ALG Sept 2109 Releng

--- profiledef.sh
+++ profiledef.sh
@@ -19,4 +19,5 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/copytoram"]="0:0:755"
 )
